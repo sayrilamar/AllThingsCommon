019ac2667f2d16e3ce3766bbfce362ab228202ef
Jarius Hayes <sayrilamar@gmail.com>
2019-02-21 00:12:33 -0500

    adds react-table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { makeData } from "./Utils.js";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: makeData()
+    };
+  }
+
   render() {
-    
+    const { data } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -13,7 +23,45 @@ class App extends Component {
         </header>
 
         <div className="App-body">
-          test
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              Header: "Name",
+              columns: [
+                {
+                  Header: "First Name",
+                  accessor: "firstName"
+                },
+                {
+                  Header: "Last Name",
+                  id: "lastName",
+                  accessor: d => d.lastName
+                }
+              ]
+            },
+            {
+              Header: "Info",
+              columns: [
+                {
+                  Header: "Total Gave",
+                  accessor: "gave"
+                }
+              ]
+            },
+            {
+              Header: 'Results',
+              columns: [
+                {
+                  Header: "Total Made",
+                  accessor: "tmade"
+                }
+              ]
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
         </div>
         
       </div>
