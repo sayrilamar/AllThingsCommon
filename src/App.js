dbd3769b6740851019296c7453e853943ce62d7b
Jarius Hayes <sayrilamar@gmail.com>
2019-02-20 23:52:56 -0500

    adds yarn

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1>All Things Common</h1>
         </header>
+
         <div className="App-body">
-          <h2> Test 1 2 3 </h2> 
+          test
         </div>
         
       </div>
